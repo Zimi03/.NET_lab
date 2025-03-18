e57448fd8db8dfe7ae591a540a20160e2e0b7846
Zimi03 <sz.senderowski@gmail.com>
2025-03-18 21:19:47 +0100

    FizzBuzz solution

---
+++ Lab0/ConsoleApp/ConsoleApp/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class FizzBuzz
+    {
+        private int HighestNum;
+        public FizzBuzz(int highestNum)
+        {
+            HighestNum = highestNum;
+        }
+
+        public void FizBuz()
+        {
+            for (int i = 1; i <= HighestNum; i++)
+            {
+                var str = "";
+                if (i % 3 == 0)
+                {
+                    str += "fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    str += "buzz";
+                }
+                if (str == "")
+                {
+                    str += i.ToString();
+                }
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
---
+++ Lab0/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz test = new FizzBuzz(20);
+            test.FizBuz();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
